b56eb2bf8c6ae25618882661c55a01fc4c307e61
MATREYcr <111471979+MATREYcr@users.noreply.github.com>
2024-06-04 09:22:48 -0500

    ..

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('WellTalkUni')
+    .setDescription('Test Endpoints')
+    .setVersion('1.0')
+    .addTag('ejemplo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  //http://localhost:4000/api#/
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
+
+
+}
+bootstrap();
